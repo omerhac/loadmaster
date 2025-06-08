ab1da80ac2f905fe6f6ce4e8f03a7a249b8cc5f0
omer <omer@eikona.io>
2025-06-08 11:57:34 +0300

    lint: fix

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,6 @@
 process.env.NODE_ENV = 'test';
 
-jest.mock('react-native', () => {
+jest.mock('react-native', () => { // eslint-disable-line no-undef
   // Create a platformSpecific store for tests to modify
   const platformMock = {
     OS: 'android',
@@ -9,9 +9,9 @@ jest.mock('react-native', () => {
   return {
     Platform: platformMock,
     StyleSheet: {
-      create: jest.fn(styles => styles),
+      create: jest.fn(styles => styles), // eslint-disable-line no-undef
     },
-    useColorScheme: jest.fn(() => 'light'),
+    useColorScheme: jest.fn(() => 'light'), // eslint-disable-line no-undef
   };
 });
 
